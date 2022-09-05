2cdba0879929bd0e328f8c628a4fd8dba3c72dc2
dagl <dagl@softwaremind.com>
2022-09-05 19:33:21 +0000

    cp metrics in deploy.sh

--- iop/deploy.sh
+++ iop/deploy.sh
@@ -1,6 +1,10 @@
 helm repo add sciencemesh https://sciencemesh.github.io/charts/
-#helm repo update
 helm dependency build
 helm upgrade -i iop  . --set-file gateway.configFiles.revad\\.toml=gateway.toml -f public-sp.toml -f home-sp.yaml -f reva-sp.yaml --set-file storageprovider-reva.configFiles.revad\\.toml=storage-reva.toml --set-file storageprovider-home.configFiles.revad\\.toml=storage-home.toml --set-file gateway.configFiles.users\\.json=users-ailleron.json --set-file storageprovider-public.configFiles.revad\\.toml=storage-public.toml # --set wopiserver.enabled=true -f wopiserver.yaml --set gateway.env.REVA_APPPROVIDER_IOPSECRET=qwerty12345 --set wopiserver.config.iopsecret=qwerty12345
-
-#helm upgrade -i iop  . --set-file gateway.configFiles.revad\\.toml=gateway.toml -f public-sp.toml -f home-sp.yaml -f reva-sp.yaml --set-file storageprovider-reva.configFiles.revad\\.toml=storage-reva.toml --set-file storageprovider-home.configFiles.revad\\.toml=storage-home.toml --set-file gateway.configFiles.users\\.json=users-ailleron.json --set-file storageprovider-public.configFiles.revad\\.toml=storage-public.toml --set wopiserver.enabled=true -f wopiserver.yaml --set gateway.env.REVA_APPPROVIDER_IOPSECRET=qwerty12345 --set wopiserver.config.iopsecret=qwerty12345
+sleep 3
+POD=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep gateway)
+if  [ ! -z "$POD" ]
+then
+kubectl exec -it $POD -- mkdir -p /var/tmp/reva/metrics/ 
+kubectl cp metricsdata.json $POD:/var/tmp/reva/metrics/metricsdata.json 
+fi
